89f8212b5d2256c2ff55b39035facd79f159da05
bdbarnett <brad.barnett@outlook.com>
2024-12-03 14:17:24 -0600

    .

--- bundle.cmake
+++
@@ -1,6 +0,0 @@
-# This file is to be given as
-#     make USER_C_MODULES=<path>/micropython.cmake
-
-
-include(${CMAKE_CURRENT_LIST_DIR}/../pydisplay_cmods/micropython.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/micropython.cmake)
